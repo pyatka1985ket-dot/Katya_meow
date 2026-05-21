70e7b6fef3e76c7b8f3a834ba53e9ec420f515ed
Ekaterina Permyakova <katperm11@yandex.ru>
2026-05-21 18:17:49 +0500

    new -2105

--- test/WB.py
+++ test/WB.py
@@ -21,6 +21,32 @@ def test_open_wb(browser):
     assert title.text == 'Присоединяйся к команде'
 
 
+def test_header(browser):
+    options = Options()
+    options.add_argument('--headless')
+    browser.get('https://career.rwb.ru/?/index.html')
+    all_vacancy = WebDriverWait(browser, 10).until(
+        EC.presence_of_element_located((By.LINK_TEXT, "Все вакансии"))
+    )
+    assert all_vacancy.text == 'Все вакансии'
+    team_link=WebDriverWait(browser, 10).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, "[data-qa='header-btn-/start']"))
+        )
+    assert team_link.text == 'Попасть в команду'
+    intern_link = WebDriverWait(browser, 10).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, "[data-qa='header-btn-/intern']"))
+    )
+    assert intern_link.text == 'Без опыта'
+    construction_link = WebDriverWait(browser, 10).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, "[data-qa='header-btn-/construction']"))
+    )
+    assert construction_link.text == 'Строительство'
+    service_link = WebDriverWait(browser, 10).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, "a[href='https://job.wb.ru/?utm_source=wb&utm_medium=career&utm_campaign=header']"))
+    )
+    assert service_link.text == 'Сервисные позиции'
+
+
 
 
 
